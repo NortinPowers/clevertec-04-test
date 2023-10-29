16cdbe3a3795ff31b6eb6566abb2d44659fd1496
NortinPowers <a.nortin@gmail.com>
2023-10-29 20:50:57 +0300

    feat: add mapper tests

---
+++ src/test/java/ru/clevertec/product/util/TestConstant.java
@@ -0,0 +1,19 @@
+package ru.clevertec.product.util;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestConstant {
+
+    public static final UUID PRODUCT_UUID = UUID.fromString("76dbb74c-2f08-4bc0-8029-aed02147e737");
+    public static final String PRODUCT_NAME = "Plumbus";
+    public static final String NEW_PRODUCT_NAME = "Portal Gun";
+    public static final String PRODUCT_DESCRIPTION = "all-purpose home device";
+    public static final String NEW_PRODUCT_DESCRIPTION = "gadget that allows the user(s) to travel between different universes/dimensions/realities";
+    public static final BigDecimal PRODUCT_PRICE = BigDecimal.TEN;
+    public static final BigDecimal NEW_PRODUCT_PRICE = BigDecimal.valueOf(11);
+    public static final LocalDateTime PRODUCT_CREATED_DATE = LocalDateTime.of(2023, 10, 28, 11, 17, 0);
+}
